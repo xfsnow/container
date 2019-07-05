ef8b47d87d7248ac70e515ab0e3060e944f5ffc2
xfsnow <xuefeng@snowpeak.org>
2019-07-05 05:24:39 +0000

    found the way to use IAM Service Role for PHP SDK credentials.

--- nginxphp/index.php
+++ nginxphp/index.php
@@ -1,4 +1,8 @@
 <?php
+use Aws\Credentials\Credentials;
+use Aws\Credentials\CredentialProvider;
+use Aws\Exception\CredentialsException;
+use EddTurtle\DirectUpload\Signature;
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
@@ -7,18 +11,15 @@ require 'vendor/autoload.php';
 $now = date("Y-m-j, H:i:s");
 echo '<h1>Hullo from webdevops/php-nginx.</h1> <h4>HOSTNAME= '.$_SERVER['HOSTNAME'].'</h4>
 <h4>It is '.$now.' now.</h4>';
-// print_r($_ENV);
-// phpinfo();
 
-
+// Use the default credential provider. This can assume IAM service role on EC2 or ECS Task.
+$provider = CredentialProvider::defaultProvider();
 $sharedConfig = [
-    'profile' => 'default',
-    'version' => 'latest',
-    'region' => 'us-east-1'
+    'region'      => 'us-east-1',
+    'version'     => 'latest',
+    'credentials' => $provider
 ];
-
-
-$s3Client = new Aws\S3\S3Client($sharedConfig);
+$s3Client = new S3Client($sharedConfig);
 $buckets = $s3Client->listBuckets();
 echo "<h4>my S3 buckets</h4><ul>";
 foreach ($buckets['Buckets'] as $bucket) {
